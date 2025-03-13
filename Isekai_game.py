f78ada1875781e75edd0a40104fee19cab45f01b
shubham <navneetsingh94520@gmail.com>
2025-03-13 18:12:45 +0530

    Modified game with several new updats

--- Isekai_game.py
+++ Isekai_game.py
@@ -4,28 +4,29 @@ class Isekai:
     def __init__(self, name):
         self.name = name
         self.level = 0
-        self.monster = ["Slime", "Goblin", "Orc", "Troll", "Stempede of monster", "Dragon"]
-        self.position = ["Traveller","Villager","Village hero","Noble","King","SSS rank adventurer"]
-        self.weapon = ["Wooden sword","Iron sword","Steel sword","Mithril sword","Excalibur"]
+        self.monster = ["Slime", "Goblin", "Orc", "Troll", "Stempede of monster", "Dragon","Demon God"]
+        self.position = ["Traveller","Villager","Village hero","Noble","King","SSS rank adventurer","Demi-God"]
+        self.weapon = ["Wooden sword","Iron sword","Steel sword","Mithril sword","Excalibur","Soul sword","Sword of light"]
         
     def hit(self):
-        print(f"{self.name} have killed the {self.monster[self.level]}!")
+        print(f"{self.name} has killed the {self.monster[self.level]}!")
         self.level += 1
-        print(f"{self.name} have leveled up!")
-
+        print(f"{self.name} has leveled up!")
+        print(f"{self.name} is now a {self.position[self.level]} with a {self.weapon[self.level]}.")
     
 player1 = Isekai(input("1. Create a character : "))
+exit_game = False
 while True:
-    print(f"Welcome to isekai! You are a {player1.name} with level {player1.level}.")
+    print(f"Welcome to isekai! You are {player1.name} with level {player1.level}.")
     print(f"{player1.name} is a {player1.position[player1.level]} with a {player1.weapon[player1.level]}.")
-   
+    
     while True:
-        if player1.level == 5:
+        if player1.level > 5:
             print(f"{player1.name} have defeated the final boss and saved the world!")
-            exit = False
+            exit_game = True
             break
         else:
-            print(f"Attention! {player1.monster[player1.level]} is infront of you!")
+            print(f"\nAttention! {player1.monster[player1.level]} is infront of you!")
             print("What do you want to do?")
             print("0. Check Status")
             print("1. Hit")
@@ -41,27 +42,35 @@ while True:
                         Sword rank : {player1.weapon[player1.level]}\n \
                         Level : {player1.level}\n ")
             elif choice == "1":
-                count = 1
+                count = 0
                 while True:
                     monster_attack = rm.randint(0,2**player1.level)
-                    try_attack = int(input(f"Enter your attack number to correctly hit him in between 0 and {2**player1.level}: "))
+                    try_attack = int(input(f"\nEnter your attack number to correctly hit him in between 0 and {2**player1.level}: "))
                                  
                     if try_attack == monster_attack:
                         player1.hit()
                         break
                     else:
-                        print("You missed!")
+                        print("Monster dodged the sword attack!\n==The monster attacked you back and you got a critical hit!==")
+                        count += 1
+                        if player1.level > 1 and count > 2**(player1.level)*0.6:
+                            print("Oops! Ultimate attack by the monster! you couldn't evade.")
+                            print(f"{player1.name} got killed by the {player1.monster[player1.level]}!")
+                            print(f"...........{player1.name} journey ends here!.........")
+                            exit()
                         
             elif choice == "2":
                 print(f"{player1.name} ran away.")
-                exit = False
+                exit_game = True
                 break
 
             elif choice == "3":
-                print(f"{player1.name} exited the game.")
-                exit = False
+                print(f"exiting the game.")
+                exit_game = True
                 break
-    if not exit:
+
+    if exit_game:
+        print(f"Thank you for playing the game {player1.name}! \nHope to meet you again!")
         break
     
 
